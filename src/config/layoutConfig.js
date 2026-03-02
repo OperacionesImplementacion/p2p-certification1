e9971c48946fe6e55f3db106658607f36e78fe55
Luis Alberto Jaime B. <39748335+luisJaimeB@users.noreply.github.com>
2026-03-02 17:55:10 -0500

    feat: [WIP] A configuration file is added to determine the fields for each certification template.

---
+++ src/config/layoutConfig.js
@@ -0,0 +1,69 @@
+export const formConfig = {
+    countries: {
+        'CO': {
+        name: 'Colombia',
+        services: {
+            'basic_payment': {
+                name: 'basic payment',
+                description: 'Servicio de pago básico para transacciones nacionales.',
+                sections: [
+                    {
+                        name: 'Cumplimiento y normativa',
+                        description: 'Detalle de las disposiciones legales, obligaciones tributarias y lineamientos operativos vigentes.',
+                        fields: ['taxes', 'terms_and_conditions', 'expiration', 'payment_summary'],
+                        icon: 'Search'
+                    },
+                    {
+                        name: 'Actualización de estados de pago',
+                        description: 'Proceso para actualizar el estado de los pagos, incluyendo la gestión de notificaciones y la integración con sistemas de seguimiento.',
+                        fields: ['cronjob', 'webhook'],
+                        icon: 'Refresh'
+                    },
+                    {
+                        name: 'Validación de datos y control de errores',
+                        description: 'Proceso de validación de datos de entrada y control de errores para garantizar la integridad del sistema.',
+                        fields: ['data_validation', 'payment_button_control'],
+                        icon: 'AlertTriangle'
+                    }
+                ],
+                icon: 'Banknote'
+            },
+            'tokenization': {
+                name: 'tokenization',
+                description: 'Servicio de tokenización para transacciones de suscripción.',
+                sections: [
+                    {
+                        name: 'Cumplimiento y normativa',
+                        description: 'Detalle de las disposiciones legales, obligaciones tributarias y lineamientos operativos vigentes.',
+                        fields: ['taxes', 'terms_and_conditions', 'expiration', 'payment_summary'],
+                        icon: 'Search'
+                    },
+                    {
+                        name: 'Actualización de estados de pago',
+                        description: 'Proceso para actualizar el estado de los pagos, incluyendo la gestión de notificaciones y la integración con sistemas de seguimiento.',
+                        fields: ['cronjob', 'webhook'],
+                        icon: 'Refresh'
+                    },
+                    {
+                        name: 'Validación de datos y control de errores',
+                        description: 'Proceso de validación de datos de entrada y control de errores para garantizar la integridad del sistema.',
+                        fields: ['data_validation', 'payment_button_control'],
+                        icon: 'AlertTriangle'
+                    }
+                ],
+                icon: 'CreditCard'
+            }
+        }
+        },
+    }
+};
+
+export const fieldDefinitions = {
+    taxes: {
+        label: 'Impuestos',
+        description: '19% IVA',
+        required: true,
+        validation: 'file',
+        icon: 'Percent'
+    },
+};
