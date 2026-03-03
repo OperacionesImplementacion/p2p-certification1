356f6e8f9f6f0e2e7a8b9fa2901e8955cd4123aa
juantestpruebas-png <juantestpruebas@gmail.com>
2026-03-03 09:42:13 -0500

    Update layoutConfig.js

--- src/config/layoutConfig.js
+++ src/config/layoutConfig.js
@@ -58,6 +58,37 @@ export const formConfig = {
                         }
                     ],
                     icon: 'CreditCard'
+                },
+                'Recurrencia': {
+                    name: 'Recurrencia',
+                    description: 'Servicio de recurrencia para transacciones ejecutadas por la pasarela.',
+                    sections: [
+                        {
+                            name: 'Cumplimiento y normativa',
+                            description: 'Detalle de las disposiciones legales, obligaciones tributarias y lineamientos operativos vigentes.',
+                            fields: ['taxes', 'terms_and_conditions', 'expiration', 'payment_summary'],
+                            icon: 'Search'
+                        },
+                        {
+                            name: 'Estructura request',
+                            description: 'Generación de petición con estructura requerida para flujo con recurrencia',
+                            fields: ['Request'],
+                            icon: 'Search'
+                        },
+                        {
+                            name: 'Actualización de estados de pago',
+                            description: 'Proceso para actualizar el estado de los pagos, incluyendo la gestión de notificaciones y la integración con sistemas de seguimiento.',
+                            fields: ['cronjob', 'webhook'],
+                            icon: 'Refresh'
+                        },
+                        {
+                            name: 'Validación de datos y control de errores',
+                            description: 'Proceso de validación de datos de entrada y control de errores para garantizar la integridad del sistema.',
+                            fields: ['data_validation', 'payment_button_control'],
+                            icon: 'AlertTriangle'
+                        }
+                    ],
+                    icon: 'CreditCard'
                 }
             }
         },
@@ -115,6 +146,37 @@ export const formConfig = {
                             name: 'Invalidación proceso de tokenización',
                             description: 'Inactivar el token generado, debido a problemas con el cobro del mismo proceder con la inactivación',
                             fields: ['Invalid_token'],
+                            icon: 'AlertTriangle'
+                        }
+                    ],
+                    icon: 'CreditCard'
+                },
+                'Recurrencia': {
+                    name: 'Recurrencia',
+                    description: 'Servicio de recurrencia para transacciones ejecutadas por la pasarela.',
+                    sections: [
+                        {
+                            name: 'Cumplimiento y normativa',
+                            description: 'Detalle de las disposiciones legales, obligaciones tributarias y lineamientos operativos vigentes.',
+                            fields: ['taxes', 'terms_and_conditions', 'expiration', 'payment_summary'],
+                            icon: 'Search'
+                        },
+                        {
+                            name: 'Estructura request',
+                            description: 'Generación de petición con estructura requerida para flujo con recurrencia',
+                            fields: ['Request'],
+                            icon: 'Search'
+                        },
+                        {
+                            name: 'Actualización de estados de pago',
+                            description: 'Proceso para actualizar el estado de los pagos, incluyendo la gestión de notificaciones y la integración con sistemas de seguimiento.',
+                            fields: ['cronjob', 'webhook'],
+                            icon: 'Refresh'
+                        },
+                        {
+                            name: 'Validación de datos y control de errores',
+                            description: 'Proceso de validación de datos de entrada y control de errores para garantizar la integridad del sistema.',
+                            fields: ['data_validation', 'payment_button_control'],
                             icon: 'AlertTriangle'
                         }
                     ],
@@ -197,6 +259,37 @@ export const formConfig = {
                         {
                             name: 'Estructura request',
                             description: 'Generación de petición con estructura requerida para flujo con recurrencia',
+                            fields: ['Request'],
+                            icon: 'Search'
+                        },
+                        {
+                            name: 'Actualización de estados de pago',
+                            description: 'Proceso para actualizar el estado de los pagos, incluyendo la gestión de notificaciones y la integración con sistemas de seguimiento.',
+                            fields: ['cronjob', 'webhook'],
+                            icon: 'Refresh'
+                        },
+                        {
+                            name: 'Validación de datos y control de errores',
+                            description: 'Proceso de validación de datos de entrada y control de errores para garantizar la integridad del sistema.',
+                            fields: ['data_validation', 'payment_button_control'],
+                            icon: 'AlertTriangle'
+                        }
+                    ],
+                    icon: 'CreditCard'
+                },
+                'Preautorización': {
+                    name: 'Preautorización',
+                    description: 'Servicio de preautorización para reservar monto al usuario y efectuar cobro posterior',
+                    sections: [
+                        {
+                            name: 'Cumplimiento y normativa',
+                            description: 'Detalle de las disposiciones legales, obligaciones tributarias y lineamientos operativos vigentes.',
+                            fields: ['taxes', 'terms_and_conditions', 'expiration', 'payment_summary'],
+                            icon: 'Search'
+                        },
+                        {
+                            name: 'Estructura request preautorización',
+                            description: 'Generación de petición con estructura requerida para Checking / Reautorización / Checkout',
                             fields: ['Request'],
                             icon: 'Search'
                         },
