e9ef6a33e50f37d35a54f1d285dde46dfd8736ec
juantestpruebas-png <juantestpruebas@gmail.com>
2026-03-03 10:01:52 -0500

    Update layoutConfig.js

--- src/config/layoutConfig.js
+++ src/config/layoutConfig.js
@@ -195,7 +195,7 @@ export const formConfig = {
                         {
                             name: 'Cumplimiento y normativa',
                             description: 'Detalle de las disposiciones legales, obligaciones tributarias y lineamientos operativos vigentes.',
-                            fields: ['taxes', 'terms_and_conditions', 'expiration', 'payment_summary'],
+                            fields: ['taxes', 'discount_law', 'terms_and_conditions', 'expiration', 'payment_summary'],
                             icon: 'Search'
                         },
                         {
@@ -220,7 +220,7 @@ export const formConfig = {
                         {
                             name: 'Cumplimiento y normativa',
                             description: 'Detalle de las disposiciones legales, obligaciones tributarias y lineamientos operativos vigentes.',
-                            fields: ['taxes', 'terms_and_conditions', 'expiration', 'payment_summary'],
+                            fields: ['taxes', 'discount_law', 'terms_and_conditions', 'expiration', 'payment_summary'],
                             icon: 'Search'
                         },
                         {
@@ -253,7 +253,7 @@ export const formConfig = {
                         {
                             name: 'Cumplimiento y normativa',
                             description: 'Detalle de las disposiciones legales, obligaciones tributarias y lineamientos operativos vigentes.',
-                            fields: ['taxes', 'terms_and_conditions', 'expiration', 'payment_summary'],
+                            fields: ['taxes', 'discount_law', 'terms_and_conditions', 'expiration', 'payment_summary'],
                             icon: 'Search'
                         },
                         {
@@ -284,7 +284,7 @@ export const formConfig = {
                         {
                             name: 'Cumplimiento y normativa',
                             description: 'Detalle de las disposiciones legales, obligaciones tributarias y lineamientos operativos vigentes.',
-                            fields: ['taxes', 'terms_and_conditions', 'expiration', 'payment_summary'],
+                            fields: ['taxes', 'discount_law', 'terms_and_conditions', 'expiration', 'payment_summary'],
                             icon: 'Search'
                         },
                         {
@@ -321,4 +321,69 @@ export const fieldDefinitions = {
         validation: 'file',
         icon: 'Percent'
     },
+    discount_law: {
+        label: 'Ley de Descuento',
+        description: 'Estructura ley de descuento',
+        required: true,
+        validation: 'file',
+        icon: 'Percent'
+    },
+    terms_and_conditions: {
+        label: 'Terminos y condiciones comercio',
+        description: 'Apartado donde se identifique terminos y condiciones aplicados en el sitio web',
+        required: false,
+        validation: 'file',
+        icon: 'Percent'
+    },
+    expiration: {
+        label: 'Tiempo de expiración',
+        description: 'Sesión de Pago no debe exceder los 30 minutos',
+        required: true,
+        validation: 'file',
+        icon: 'Percent'
+    },
+    payment_summary: {
+        label: 'Resumen de pago transacción',
+        description: 'En la interfaz del comercio se debe mostrar como minimo estado, referencia, fecha, valor de la transacción',
+        required: true,
+        validation: 'file',
+        icon: 'Percent'
+    },
+    Request: {
+        label: 'Request de petición generada',
+        description: 'Se espera estructura request enviada en la petición de creación de sesión',
+        required: true,
+        validation: 'file',
+        icon: 'Percent'
+    },
+    cronjob: {
+        label: 'CronJob',
+        description: 'Tarea programada con ejecuación cada 24 horas',
+        required: true,
+        validation: 'file',
+        icon: 'Percent'
+    },
+    webhook: {
+        label: 'Webhook',
+        description: 'URL de notificación',
+        required: true,
+        validation: 'file',
+        icon: 'Percent'
+    },
+    data_validation: {
+        label: 'Nombres - Apellidos - Documento - Email',
+        description: 'Se requiere que comercio realice validación de campos de la información previamente ingresada por el usuario',
+        required: true,
+        validation: 'file',
+        icon: 'Percent'
+    },
+    payment_button_control:{
+        label: 'Botón de redirección',
+        description: 'Se requiere que se controle el evento de redirección hacia el Webcheckout',
+        required: true,
+        validation: 'file',
+        icon: 'Percent'
+    }
+
+
 };
